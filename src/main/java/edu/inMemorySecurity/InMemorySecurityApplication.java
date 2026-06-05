9bce4e9504afc17426c8bba55adfc59ac15a2c6e
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-06-05 14:36:05 -0300

    First commit

---
+++ src/main/java/edu/inMemorySecurity/InMemorySecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.inMemorySecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemorySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemorySecurityApplication.class, args);
+	}
+
+}
